335e25cdc268e33befecd3876d8b941c7a524544
Balazs Kertesz <kertesz.balazs92@gmail.com>
2020-05-19 20:44:40 +0200

    Added sleep method

--- binary_clock.ino
+++ binary_clock.ino
@@ -2,14 +2,19 @@
 #define DEBOUNCE 150
 #define MAX_HOUR 12
 #define MAX_MINUTE 60
+#define SLEEP_DELAY 2
 
-int minute;
-int hour;
-bool leftButton;
-bool rightButton;
 const long interval = 60000;
 unsigned long previousMillis;
 
+int minute;
+int hour;
+int sleepTimer;
+
+bool leftButton;
+bool rightButton;
+bool slideSwitch;
+bool isSleeping;
 
 void setup() {
   CircuitPlayground.begin();
@@ -17,6 +22,8 @@ void setup() {
   minute = 0;
   hour = 0;
   previousMillis = 0;
+  isSleeping = false;
+  sleepTimer = SLEEP_DELAY;
 }
 
 void loop() {
@@ -55,12 +62,17 @@ void setPixel(int pixelNumber, int colorCode) {
 void checkButtonPress() {
   leftButton = CircuitPlayground.leftButton();
   rightButton = CircuitPlayground.rightButton();
-  if(leftButton) {
+  slideSwitch = CircuitPlayground.slideSwitch();
+  if(!slideSwitch && (leftButton || rightButton)) {
+    isSleeping = false;
+    sleepTimer = SLEEP_DELAY;
+  }
+  if(leftButton && slideSwitch) {
     hour++;
     redrawFace();
     delay(DEBOUNCE);
   }
-  if(rightButton) {
+  if(rightButton && slideSwitch) {
     minute++;
     redrawFace();
     delay(DEBOUNCE);
@@ -73,6 +85,7 @@ void checkTime() {
   if(currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     minute ++;
+    checkSleep();
     redrawFace();
   }
   
@@ -89,8 +102,23 @@ void checkTimeOverflow() {
 void redrawFace() {
   checkTimeOverflow();
   CircuitPlayground.clearPixels();
-  displayBinary(minute, 1);
-  displayBinary(hour, 2, true);
+  if(!isSleeping) {
+    displayBinary(minute, 1);
+    displayBinary(hour, 2, true);
+  }
+  printTime();
+}
+
+void printTime() {
   String currentTime = (String) hour + ':' +  (String) minute;
   Serial.println(currentTime);
+}
+
+void checkSleep() {
+  if(!isSleeping && sleepTimer > 0) {
+    sleepTimer--;
+  } else {
+    sleepTimer = SLEEP_DELAY;
+    isSleeping = true;
+  }
 }
